afb589c360c6f52919eb25b6654a7ae04ead416c
kalush666 <jonathan.kalush@gmail.com>
2025-08-11 12:11:51 +0300

    Feat: Integrate networking configuration into application startup

--- TelemetryDevice/Program.cs
+++ TelemetryDevice/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.Extensions.Options;
+using TelemetryDevice.Common;
+using TelemetryDevice.Config;
 using TelemetryDevice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddLogging();
 builder.Services.AddOpenApi();
 
-builder.Services.AddSingleton<IPacketSniffer,PacketSniffer>();
+builder.Services.Configure<NetworkingConfiguration>(
+    builder.Configuration.GetSection(TelemetryDeviceConstants.Configuration.NETWORKING_SECTION));
+
+builder.Services.AddSingleton<IPacketSniffer, PacketSniffer>();
 
 var app = builder.Build();
 
