c1635d3f7aa36816fb8dbaf7d80e009b750cea53
kalush666 <jonathan.kalush@gmail.com>
2025-08-11 12:11:51 +0300

    Feat: Introduce networking configuration model

--- TelemetryDevice/Config/NetworkingConfiguration.cs
+++ TelemetryDevice/Config/NetworkingConfiguration.cs
@@ -2,5 +2,15 @@
 {
     public class NetworkingConfiguration
     {
+        public List<string> Protocols { get; set; } = new();
+        public List<string> Interfaces { get; set; } = new();
+
+        public NetworkingConfiguration() { }
+
+        public NetworkingConfiguration(List<string> protocols, List<string> interfaces)
+        {
+            Protocols = protocols;
+            Interfaces = interfaces;
+        }
     }
 }
