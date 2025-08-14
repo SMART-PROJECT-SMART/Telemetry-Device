44e5bf063740a75fa5585275f9e6682aa5873898
kalush666 <jonathan.kalush@gmail.com>
2025-08-14 09:20:19 +0300

    create telemetry device dto

--- TelemetryDevice/Dto/CreateTelemetryDeviceDto.cs
+++ TelemetryDevice/Dto/CreateTelemetryDeviceDto.cs
@@ -1,6 +1,18 @@
-﻿namespace TelemetryDevices.Dto
+﻿using TelemetryDevices.Models;
+
+namespace TelemetryDevices.Dto
 {
     public class CreateTelemetryDeviceDto
     {
+        public int TailId { get; set; }
+        public List<int> PortNumbers { get; set; }
+        public Location Location { get; set; }
+        public CreateTelemetryDeviceDto() { }
+        public CreateTelemetryDeviceDto(int tailId, List<int> portNumbers, Location location)
+        {
+            TailId = tailId;
+            PortNumbers = portNumbers;
+            Location = location;
+        }
     }
 }
