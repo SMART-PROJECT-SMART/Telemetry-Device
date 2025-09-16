317bc74d286757a9c1947facba29fa18d473d69d
kalush666 <jonathan.kalush@gmail.com>
2025-09-16 16:18:41 +0300

    [improve] removing init state

--- TelemetryDevice/Program.cs
+++ TelemetryDevice/Program.cs
@@ -32,20 +32,4 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 app.MapControllers();
 
-var sniffer = app.Services.GetRequiredService<IPacketSniffer>();
-sniffer.AddPort(TelemetryDeviceConstants.DefaultValues.DEFAULT_PORT_1);
-sniffer.AddPort(TelemetryDeviceConstants.DefaultValues.DEFAULT_PORT_2);
-
-var tdManager = app.Services.GetRequiredService<TelemetryDeviceManager>();
-tdManager.AddTelemetryDevice(
-    TelemetryDeviceConstants.DefaultValues.DEFAULT_TAIL_ID,
-    [
-        TelemetryDeviceConstants.DefaultValues.DEFAULT_PORT_1,
-        TelemetryDeviceConstants.DefaultValues.DEFAULT_PORT_2,
-    ],
-    new Location(
-        TelemetryDeviceConstants.DefaultValues.DEFAULT_LOCATION_LAT,
-        TelemetryDeviceConstants.DefaultValues.DEFAULT_LOCATION_LON
-    )
-);
 app.Run();
