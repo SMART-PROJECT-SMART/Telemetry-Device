c8f491d5c06e3b33a5868432d024a6fced9b8086
kalush666 <jonathan.kalush@gmail.com>
2025-10-09 09:14:40 +0300

    [feat] telemetry  device status job

--- TelemetryDevice/Services/Quartz/Jobs/TelemetryDeviceStatusJob.cs
+++ TelemetryDevice/Services/Quartz/Jobs/TelemetryDeviceStatusJob.cs
@@ -1,5 +1,7 @@
 ﻿using Quartz;
+using TelemetryDevices.Models;
 using TelemetryDevices.Services.Kafka.Producers;
+using TelemetryDevices.Services.Kafka.Producers.TelemetryDevicesStatusProducer;
 using TelemetryDevices.Services.TelemetryDevicesManager;
 
 namespace TelemetryDevices.Services.Quartz.Jobs
@@ -7,7 +9,19 @@ namespace TelemetryDevices.Services.Quartz.Jobs
     public class TelemetryDeviceStatusJob : IJob
     {
         private readonly ITelemetryDeviceManager _telemetryDeviceManager;
+        private readonly ITelemetryDeviceStatusProducer _telemetryDeviceStatusProducer;
 
-        public Task Execute(IJobExecutionContext context) { }
+        public TelemetryDeviceStatusJob(ITelemetryDeviceManager telemetryDeviceManager,ITelemetryDeviceStatusProducer telemetryDeviceStatusProducer)
+        {
+            _telemetryDeviceManager = telemetryDeviceManager;
+            _telemetryDeviceStatusProducer = telemetryDeviceStatusProducer;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            IEnumerable<TelemetryDevice> telemetryDevices = _telemetryDeviceManager.GetAllTelemetryDevices();
+            _telemetryDeviceStatusProducer.ProduceAsync(telemetryDevices);
+            return Task.CompletedTask;
+        }
     }
 }
